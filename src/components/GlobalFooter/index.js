d62c7b670e1cdafbdb67ce836604c92f907a7574
dancerphil <413149044@qq.com>
2017-11-11 14:10:52 +0800

    :art: using antd row col instead of bootstrap

--- src/components/GlobalFooter/index.js
+++ src/components/GlobalFooter/index.js
@@ -1,37 +1,38 @@
 import React from 'react';
+import { Col, Row } from 'antd';
 
 export default () => {
   return (
     <div>
-      <div className="p-5 text-center text-white" stylex="background-color: #05585C; user-select: none">
+      <div className="p-5 text-center text-white" style={{ backgroundColor: '#05585C', userSelect: 'none', color: 'white' }}>
         <div className="container">
-          <div className="row">
-            <div className="col">
+          <Row gutter={24}>
+            <Col span={8}>
               <p><i className="fa fa-2x fa-users" aria-hidden="true" /></p>
               <h4><strong><big>23864</big></strong>位用户</h4>
-            </div>
-            <div className="col">
+            </Col>
+            <Col span={8}>
               <p><i className="fa fa-2x fa-comments" aria-hidden="true" /></p>
               <h4><strong><big>36548</big></strong>次讨论</h4>
-            </div>
-            <div className="col">
+            </Col>
+            <Col span={8}>
               <p><i className="fa fa-2x fa-code" aria-hidden="true" /></p>
               <h4><strong><big>65371</big></strong>份代码</h4>
-            </div>
-          </div>
+            </Col>
+          </Row>
         </div>
       </div>
-      <div className="p-5 text-secondary" stylex="background-color: #252525; user-select: none">
+      <div className="p-5 text-secondary" style={{ backgroundColor: '#252525', userSelect: 'none', color: 'white' }}>
         <div className="container">
-          <div className="row">
-            <div className="col">
+          <Row gutter={24}>
+            <Col span={8}>
               <small>CODEIT是一个优秀的在线编程学习网站。我们准备了精心打造的图文教程，并且配备了相应的习题。在学习的过程中，倘
                 若遇到困难，用户可以发表帖子到讨论区与其它用户共同交流。此外，平台也准备了一批在线的一对一真人指导，用户可以在
                 任何界面进行预约，对于新手学员来说，他们不必折腾复杂的开发环境，编写好的代码可以直接在网页上运行，或者保存到服
                 务器上。我们真诚希望你可以享受在CODEIT上学习的每一分钟。
               </small>
-            </div>
-            <div className="col ml-5">
+            </Col>
+            <Col span={8}>
               <small>纽佰瑞编程教育有限公司</small>
               <br /><br />
               <small>办公地址：上海市杨浦区复旦大学创新创业学院308</small>
@@ -42,8 +43,8 @@ export default () => {
               <br /><br />
               <small>微信公众号：纽佰瑞编程</small>
               <br /><br />
-            </div>
-            <div className="col">
+            </Col>
+            <Col span={8}>
               <form action="#">
                 <div className="form-group">
                   <input
@@ -64,11 +65,11 @@ export default () => {
                 </div>
                 <button type="submit" className="btn btn-secondary btn-block btn-sm">确认提交</button>
               </form>
-            </div>
-          </div>
+            </Col>
+          </Row>
         </div>
       </div>
-      <div className="p-5 text-secondary text-center" stylex="background-color: #1C1C1C; user-select: none">
+      <div className="p-5 text-secondary text-center" style={{ backgroundColor: '#1C1C1C', userSelect: 'none', color: 'white' }}>
         <small>Newbrain Inc. 2016-2017 all right reserved. | 沪ICP备17026082号</small>
       </div>
     </div>
