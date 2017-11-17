68cb0f0c5e95a43b47b81a6f30a37c0e8baa3e6a
dancerphil <413149044@qq.com>
2017-11-17 17:24:45 +0800

    :sparkles: header struct

--- src/common/nav.js
+++ src/common/nav.js
@@ -6,29 +6,6 @@ import Analysis from '../routes/HomePage/index';
 import Monitor from '../routes/Dashboard/Monitor';
 import Workplace from '../routes/Dashboard/Workplace';
 
-import TableList from '../routes/List/TableList';
-import CoverCardList from '../routes/List/CoverCardList';
-import CardList from '../routes/List/CardList';
-import FilterCardList from '../routes/List/FilterCardList';
-import SearchList from '../routes/List/SearchList';
-import BasicList from '../routes/List/BasicList';
-
-import BasicProfile from '../routes/Profile/BasicProfile';
-import AdvancedProfile from '../routes/Profile/AdvancedProfile';
-
-import BasicForm from '../routes/Forms/BasicForm';
-import AdvancedForm from '../routes/Forms/AdvancedForm';
-import StepForm from '../routes/Forms/StepForm';
-import Step2 from '../routes/Forms/StepForm/Step2';
-import Step3 from '../routes/Forms/StepForm/Step3';
-
-import Exception403 from '../routes/Exception/403';
-import Exception404 from '../routes/Exception/404';
-import Exception500 from '../routes/Exception/500';
-
-import Success from '../routes/Result/Success';
-import Error from '../routes/Result/Error';
-
 import Login from '../routes/User/Login';
 import Register from '../routes/User/Register';
 import RegisterResult from '../routes/User/RegisterResult';
@@ -39,118 +16,20 @@ const data = [{
   name: '首页', // for breadcrumb
   path: '',
   children: [{
-    name: 'Dashboard',
+    name: '分析页',
+    path: 'analysis',
     icon: 'dashboard',
-    path: 'dashboard',
-    children: [{
-      name: '分析页',
-      path: 'analysis',
-      component: Analysis,
-    }, {
-      name: '监控页',
-      path: 'monitor',
-      component: Monitor,
-    }, {
-      name: '工作台',
-      path: 'workplace',
-      component: Workplace,
-    }],
+    component: Analysis,
   }, {
-    name: '表单页',
-    path: 'form',
+    name: '监控页',
+    path: 'monitor',
     icon: 'form',
-    children: [{
-      name: '基础表单',
-      path: 'basic-form',
-      component: BasicForm,
-    }, {
-      name: '分步表单',
-      path: 'step-form',
-      component: StepForm,
-      children: [{
-        path: 'confirm',
-        component: Step2,
-      }, {
-        path: 'result',
-        component: Step3,
-      }],
-    }, {
-      name: '高级表单',
-      path: 'advanced-form',
-      component: AdvancedForm,
-    }],
+    component: Monitor,
   }, {
-    name: '列表页',
-    path: 'list',
+    name: '工作台',
+    path: 'workplace',
     icon: 'table',
-    children: [{
-      name: '查询表格',
-      path: 'table-list',
-      component: TableList,
-    }, {
-      name: '标准列表',
-      path: 'basic-list',
-      component: BasicList,
-    }, {
-      name: '卡片列表',
-      path: 'card-list',
-      component: CardList,
-    }, {
-      name: '搜索列表（项目）',
-      path: 'cover-card-list',
-      component: CoverCardList,
-    }, {
-      name: '搜索列表（应用）',
-      path: 'filter-card-list',
-      component: FilterCardList,
-    }, {
-      name: '搜索列表（文章）',
-      path: 'search',
-      component: SearchList,
-    }],
-  }, {
-    name: '详情页',
-    path: 'profile',
-    icon: 'profile',
-    children: [{
-      name: '基础详情页',
-      path: 'basic',
-      component: BasicProfile,
-    }, {
-      name: '高级详情页',
-      path: 'advanced',
-      component: AdvancedProfile,
-    }],
-  }, {
-    name: '结果',
-    path: 'result',
-    icon: 'check-circle-o',
-    children: [{
-      name: '成功',
-      path: 'success',
-      component: Success,
-    }, {
-      name: '失败',
-      path: 'fail',
-      component: Error,
-    }],
-  }, {
-    name: '异常',
-    path: 'exception',
-    icon: 'warning',
-    children: [{
-      name: '403',
-      path: '403',
-      component: Exception403,
-    }, {
-      name: '404',
-      path: '404',
-      component: Exception404,
-    }, {
-      name: '500',
-      path: '500',
-      component: Exception500,
-    }],
+    component: Workplace,
   }],
 }, {
   component: UserLayout,
