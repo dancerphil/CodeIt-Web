482cf195fe29a87aed11d94b1a03ea5a255a7138
dancerphil <413149044@qq.com>
2017-11-21 11:13:50 +0800

    :art: add class card action

--- src/routes/Class/ClassCard.js
+++ src/routes/Class/ClassCard.js
@@ -1,11 +1,17 @@
 import React, { PureComponent } from 'react';
 import { Card } from 'antd';
+import Action from './Action';
 
 export default class ClassCard extends PureComponent {
   render() {
     const { item } = this.props;
+    const actions = [
+      <Action name="人数" value={item.learners} />,
+      <Action name="课程" value={item.lessons} />,
+      <Action name="测验" value={item.quizzes} />,
+    ];
     return (
-      <Card hoverable actions={[<a>{item.learners}</a>, <a>{item.lessons}</a>, <a>{item.quizzes}</a>]}>
+      <Card hoverable actions={actions}>
         <Card.Meta
           avatar={<img alt="" src={item.icon} />}
           title={<a href="#">{item.name}</a>}
