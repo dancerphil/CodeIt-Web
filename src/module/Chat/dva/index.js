2e1cf1f9937b00009d55fb523438f0cdca14c8f1
张聪 <413149044@qq.com>
2017-12-10 18:05:04 +0800

    try join room

--- src/module/Chat/dva/index.js
+++ src/module/Chat/dva/index.js
@@ -1,4 +1,21 @@
+import io from 'socket.io-client';
+import { applyPatch, createPatch } from 'diff';
 import { room } from '../../../services/api';
+
+const dealWith = (str) => {
+  console.log(`dealing with: ${str}`);
+  const l = str.length;
+  if (l === 0) {
+    console.log('returns: \\r\\n');
+    return '\r\n';
+  }
+  if (str.slice(l - 1, l) !== '\n') {
+    console.log('returns: raw + \\r\\n');
+    return `${str}\r\n`;
+  }
+  console.log('returns: raw');
+  return str;
+};
 
 export default {
   namespace: 'chat',
@@ -10,12 +27,16 @@ export default {
     *get(_, { call, put }) {
       const response = yield call(room);
       yield put({
-        type: 'getSuccess',
+        type: 'init',
         payload: response,
       });
     },
-    *create(_, { put }) {
+    *create({ payload }, { put }) {
       try {
+        yield put({
+          type: 'socketJoin',
+          payload: payload.title,
+        });
         yield put({
           type: 'router/set',
           payload: 'chat/playground',
@@ -24,11 +45,48 @@ export default {
         console.log(e); // eslint-disable-line
       }
     },
+    *value({ payload }, { put }) {
+      yield put({
+        type: 'socketValue',
+        payload: payload.value,
+      });
+    },
   },
 
   reducers: {
-    getSuccess(state, action) {
-      return Object.assign({}, state, action.payload);
+    init(state, action) {
+      const socket = io();
+      console.log('create socket', socket);
+      socket.on('code', (msg) => {
+        console.log(`applyPatch(state.text, msg): ${applyPatch(this.props.code.content, msg)}`);
+        const text = applyPatch(this.props.code.content, msg);
+        if (text) {
+          this.props.dispatch({
+            type: 'code/set',
+            payload: { content: dealWith(text) },
+          });
+        }
+      });
+      return Object.assign({}, state, action.payload, { socket });
+    },
+    socketJoin(state, action) {
+      const { socket } = state;
+      if (socket) {
+        console.log(`socket.emit('join', action.payload: ${action.payload})`);
+        socket.emit('join', action.payload);
+      } else {
+        console.log('no socket');
+      }
+    },
+    socketValue(state, action) {
+      const value = dealWith(action.payload);
+      const { socket } = state;
+      if (socket) {
+        console.log(`createPatch('left', text, newValue)): ${createPatch('left', this.props.code.content, value)}`);
+        socket.emit('code', createPatch('code', this.props.code.content, value));
+      } else {
+        console.log('no socket');
+      }
     },
   },
 };
--- src/module/Chat/view/index.js
+++ src/module/Chat/view/index.js
@@ -1,59 +1,14 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import io from 'socket.io-client';
-import { applyPatch, createPatch } from 'diff';
 import Playground from '../../Playground/view/index';
 
-const dealWith = (str) => {
-  console.log(`dealing with: ${str}`);
-  const l = str.length;
-  if (l === 0) {
-    console.log('returns: \\r\\n');
-    return '\r\n';
-  }
-  if (str.slice(l - 1, l) !== '\n') {
-    console.log('returns: raw + \\r\\n');
-    return `${str}\r\n`;
-  }
-  console.log('returns: raw');
-  return str;
-};
-
-@connect(state => ({
-  code: state.code,
-}))
+@connect()
 export default class Chat extends PureComponent {
-  state = {
-    socket: {},
-  }
-  componentDidMount() {
-    const socket = io();
+  handleTextChange = (value) => {
     this.props.dispatch({
-      type: 'code/set',
-      payload: { content: dealWith('') },
+      type: 'chat/valueChange',
+      payload: { value },
     });
-    socket.on('left', (msg) => {
-      console.log(`applyPatch(state.text, msg): ${applyPatch(this.props.code.content, msg)}`);
-      const text = applyPatch(this.props.code.content, msg);
-      if (text) {
-        this.props.dispatch({
-          type: 'code/set',
-          payload: { content: dealWith(text) },
-        });
-      }
-    });
-    this.state.socket = socket;
-  }
-
-  handleTextChange = (newValue) => {
-    const value = dealWith(newValue);
-    const { socket } = this.state;
-    if (socket) {
-      console.log(`createPatch('left', text, newValue)): ${createPatch('left', this.props.code.content, value)}`);
-      socket.emit('left', createPatch('left', this.props.code.content, value));
-    } else {
-      console.log('no socket');
-    }
   }
 
   render() {
